a34e9d05096a0149a8f401e140696997afe6308b
r-vijay-r <503r.vijay@gmail.com>
2016-10-11 17:27:45 +0530

    updated on 11/10/2016 5:27PM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  message = 'This is a new message';
+  users=[
+  {id:25, name:'Chris', username:'christofer'},
+  {id:25, name:'Boban', username:'bobans'},
+  {id:25, name:'Mijo', username:'jhonsan'},
+  {id:25, name:'Aswin', username:'asjin'},
+  {id:25, name:'Jabar', username:'mohammed'}
+  ];
+  activeuser;
+  selectUser(user){
+  	this.activeuser=user;
+  	console.log(this.activeuser);
+  }
 }
