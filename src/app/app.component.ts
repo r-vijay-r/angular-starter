2e0fc0829dea8f0b276bb32b58d44fc1b529cd2b
r-vijay-r <503r.vijay@gmail.com>
2016-10-12 11:53:00 +0530

    Working with a single component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { user } from './shared/models/user';
+import { User } from './shared/models/user';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,14 +8,14 @@ import { user } from './shared/models/user';
 export class AppComponent {
   title = 'app works!';
   message = 'This is a new message';
-  users: user[]=[
+  users: User[]=[
   {id:25, name:'Chris', username:'christofer'},
   {id:25, name:'Boban', username:'bobans'},
   {id:25, name:'Mijo', username:'jhonsan'},
   {id:25, name:'Aswin', username:'asjin'},
   {id:25, name:'Jabar', username:'mohammed'}
   ];
-  activeuser=user;
+  activeuser:User;
   selectUser(user){
   	this.activeuser=user;
   	console.log(this.activeuser);
