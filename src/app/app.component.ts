bc363c63e5d55919e93ee0b6e4bb0ca61b35bb9c
r-vijay-r <503r.vijay@gmail.com>
2016-10-14 11:17:13 +0530

    firebase working correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { User } from './shared/models/user';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -7,21 +8,35 @@ import { User } from './shared/models/user';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
-  message = 'This is a new message';
-  users: User[]=[
-  {id:25, name:'Chris', username:'christofer'},
-  {id:25, name:'Boban', username:'bobans'},
-  {id:25, name:'Mijo', username:'jhonsan'},
-  {id:25, name:'Aswin', username:'asjin'},
-  {id:25, name:'Jabar', username:'mohammed'}
-  ];
-  activeuser:User;
-  selectUser(user){
-  	this.activeuser=user;
-  	console.log(this.activeuser);
+  title = 'Welcome to the app!';
+  message = 'This is a Sample App';
+  items:FirebaseListObservable<any[]>;
+  activeuser: User;
+  constructor(af: AngularFire) {
+    this.items = af.database.list('/UserList');
+  }
+
+  updateItem(event) {
+    this.items.update(event.user.id, { name: event.user.name });
+  }
+  deleteItem(key: string) {    
+    this.items.remove(key); 
+  }
+  deleteEverything() {
+    this.items.remove();
+  }
+  selectUser(key: string, name:string, username: string){
+   // this.activeuser.id=key;
+    this.activeuser={id: key, name:name, username:username};
+    console.log(this.activeuser.name);
+  	//console.log(this.activeuser);
   }
   onUserCreated(event){
-    this.users.push(event.user);
+   this.items.push({ name: event.user.name ,username: event.user.username});
   }
+  conso(key: string, name:string){
+    console.log(key);
+    console.log(name);
+  }
+
 }
